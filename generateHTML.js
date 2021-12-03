6c0ddd8b21d88b698d555984d3e6fbc221a52ed5
Olivia Hall <ohall1223@gmail.com>
2021-12-03 15:03:33 -0700

    generate profiles written

--- generateHTML.js
+++ generateHTML.js
@@ -28,15 +28,60 @@ function generateProfile(team) {
 const generateManagerProfile = (Manager) => {
     return `
     <div class="card" style="width: 18rem">
-    <div class="card-header">
-        <h3 class="card-title">${Manager.getName()}<h3>
-        <h6 class="card-text">${Manager.getRole()}<h6>
+        <div class="card-header">
+            <h3 class="card-title">${Manager.getName()}<h3>
+            <h6 class="card-text">${Manager.getRole()}<h6>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${Manager.getId()}<li>
+                <li class="list-group-item">Email: ${Manager.getEmail()}<li>
+                <li class="list-group-item">Office Number: ${Manager.getOfficeNumber()}<li>
+            </ul>
+        </div>
     </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${Manager.getId()}<li>
-            <li class="list-group-item">Email: ${Manager.getEmail()}<li>
-            <li class="list-group-item">Office Number: ${Manager.getOfficeNumber()}<li>
-            
+    `
+};
+
+const generateEngineerProfile = (Engineer) => {
+    return `
+    <div class="card" style="width: 18rem">
+        <div class="card-title">
+            <h3 class="card-title"> ${Engineer.getName()}</h3>
+            <h6 class="card-text"> ${Engineer.getRole()}</h6>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${Engineer.getID()}</li>
+                <li class="list-group-item">Email: ${Engineer.getEmail()}</li>
+                <li class="list-group-item">GitHub: ${Engineer.getGithub()}</li>
+            </ul>
+        </div>
+    </div>
     `
 }
+
+const generateInternProfile = (Intern) => {
+    return `
+    <div class="card" style="width: 18rem">
+        <div class="card-title">
+            <h3 class="card-title"> ${Intern.getName()}</h3>
+            <h6 class="card-text"> ${Intern.getRole()}</h6>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${Intern.getID()}</li>
+                <li class="list-group-item">Email: ${Intern.getEmail()}</li>
+                <li class="list-group-item">School: ${Intern.gerSchool()}</li>
+            </ul>
+        </div>
+    </div>
+    `
+}
+
+function generateTeam(team) {
+    console.log(team)
+
+}
+
+module.exports = generateTeam;
